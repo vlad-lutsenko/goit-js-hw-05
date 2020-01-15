5d86a7ab80ece455cd35590e2fe49e06b1404682
Vlad Lutsenko <lutsenko.vladyslav@gmail.com>
2020-01-15 18:17:53 +0200

    t-3

---
+++ task-3.js
@@ -0,0 +1,34 @@
+"use strict";
+class Storage {
+  constructor(arr) {
+    this.items = arr;
+  }
+  getItems() {
+    return this.items;
+  }
+  addItem(item) {
+    if (!this.items.includes(item)) {
+      this.items.push(item);
+    }
+  }
+  removeItem(item) {
+    if (this.items.includes(item)) {
+      this.items.splice(this.items.indexOf(item), 1);
+    }
+  }
+}
+
+const storage = new Storage([
+  "Нанитоиды",
+  "Пролонгер",
+  "Железные жупи",
+  "Антигравитатор"
+]);
+const items = storage.getItems();
+console.table(items);
+
+storage.addItem("Дроид");
+console.table(storage.items);
+
+storage.removeItem("Пролонгер");
+console.table(storage.items);
